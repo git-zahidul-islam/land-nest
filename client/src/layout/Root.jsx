0d3bf003ad7a13c92d47efc8817e86972515b06c
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-10-04 15:46:06 +0600

    navbar added

--- client/src/layout/Root.jsx
+++ client/src/layout/Root.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../shared/Navbar";
 
 const Root = () => {
     return (
         <div>
-            <p>nav</p>
+            <Navbar/>
             <Outlet/>
             <p>footer</p>
         </div>
