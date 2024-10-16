64ae4d2d23434957de77b9d85f7b308254289ff2
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-10-16 12:42:18 +0600

    banner fix

--- client/src/pages/home/banner/Banner.jsx
+++ client/src/pages/home/banner/Banner.jsx
@@ -4,14 +4,17 @@ const images = [
   {
     src: "https://img.freepik.com/premium-photo/modern-apartment-building-exterior-sunrise-perspective-view-modern-apartment-building-exterio_636537-279673.jpg?w=900",
     text: "Modern Apartment Building",
+    stats: { views: "1.2k", likes: "350", location: "New York" },
   },
   {
     src: "https://img.freepik.com/free-photo/analog-landscape-city-with-buildings_23-2149661456.jpg?t=st=1728036926~exp=1728040526~hmac=d04c41219b974fd0b6538c957d45c6ff7518028031ccd08146a9bdd6e3917d83&w=900",
     text: "Cityscape with Buildings",
+    stats: { views: "1.2k", likes: "350", location: "New York" },
   },
   {
     src: "https://img.freepik.com/free-photo/3d-rendering-house-model_23-2150799725.jpg?t=st=1728041452~exp=1728045052~hmac=791f57ada1789729513b7b5fc362a9a111ab2d0d6f1df7a08d3880eb13d8e2e2&w=740",
     text: "3D House Model Rendering",
+    stats: { views: "1.2k", likes: "350", location: "New York" },
   },
 ];
 
@@ -44,12 +47,20 @@ const Banner = () => {
               />
             </div>
           ))}
+          
+          {/* stats is here */}
+          <div className="absolute bottom-5 right-5 text-white bg-black bg-opacity-50 p-1 rounded-lg flex md:flex-col flex-row md:gap-0 gap-3 md:justify-end justify-center">
+            <p>Views: {images[currentIndex].stats.views}</p>
+            <p>Likes: {images[currentIndex].stats.likes}</p>
+            <p>Location: {images[currentIndex].stats.location}</p>
+          </div>
+
         </div>
 
         <section className="md:w-[80%] w-full mx-auto z-50 absolute md:top-1/2 top-[30%] md:rounded-s-2xl md:rounded-e-2xl md:px-0 px-5">
-          <div className="flex items-center md:flex-row flex-col">
+          <div className="flex items-center justify-center md:flex-row flex-col">
             <input
-              className="md:text-lg text-base md:w-[40%] w-full md:rounded-s-2xl p-[11.5px] bg-opacity-80 bg-white"
+              className="md:text-lg text-base md:w-[40%] w-full md:rounded-s-2xl p-[11px] bg-opacity-80 bg-white outline-none"
               placeholder="search"
               type="search"
               name="search"
