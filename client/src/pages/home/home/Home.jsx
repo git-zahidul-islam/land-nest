3dd969dbaa6300397afb01a881e986ca58ca9f77
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-10-28 19:45:27 +0600

    some change in home

--- client/src/pages/home/home/Home.jsx
+++ client/src/pages/home/home/Home.jsx
@@ -12,6 +12,7 @@ const Home = () => {
             <Suspense fallback={<div className="h-full w-full bg-green-400 transition-all animate-bounce duration-700"></div>}>
                 <BestListing/>         
             </Suspense>
+            {/* give another section */}
         </div>
     );
 };
